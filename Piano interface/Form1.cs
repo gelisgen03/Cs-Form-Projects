9a8dca731f70aea527109165ed78765627f9a907
gelisgen03 <gelisgen987@gmail.com>
2024-12-17 00:37:53 +0300

    refresh

---
+++ Piano interface/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab9_12_24
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            int artis = 0;
+            int artis2 = 0;
+            int bosluk = 2;
+
+            for (int i = 0; i < 21; i++)
+            {
+                if (i % 7 == 2 || i % 7 == 6)
+
+                {
+                    artis2 += 40;
+                    continue;
+                    
+            
+                }
+                Button button = new Button();
+                button.Text = i.ToString();
+                button.Location = new Point(30 + artis2, 10);
+                button.Size = new Size(40, 40);
+                button.BackColor = Color.Black;
+                button.ForeColor = Color.Black;
+                button.Visible = true;
+                button.BringToFront();
+                this.Controls.Add(button);
+                artis2 += 40;
+
+
+
+
+            }
+
+            for (int i=0;i<21;i++)
+            {
+                Button button = new Button();
+                button.Text = i.ToString();
+                button.Location = new Point(10+artis, 10);
+                button.Size = new Size(40, 90);
+                button.BackColor = Color.Turquoise;
+                button.ForeColor = Color.Black;
+                button.Visible = true;
+                button.BringToFront();
+                this.Controls.Add(button);
+                artis += 40;
+
+            }
+
+           
+
+
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Piano interface/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("lab9-12-24")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("lab9-12-24")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("3ced6d36-36c3-46e7-bea9-fc0a52f8b8f2")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
